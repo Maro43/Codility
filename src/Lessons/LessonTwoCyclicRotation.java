1e794400625d3b72d47706f4da04d9ffaf04174f
Maro 43 <mariusz.olbrys@outlook.com>
2023-04-20 11:58:21 +0200

    Lesson Two

--- src/Lessons/LessonTwoCyclicRotation.java
+++ src/Lessons/LessonTwoCyclicRotation.java
@@ -1,14 +1,24 @@
 package Lessons;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 
 public class LessonTwoCyclicRotation {
     public static void main(String[] args) {
 
-        int[] A = new int[]{4, 5, 7, 8, 9};
-        int K = 3;
-        ArrayList<Object> list = new ArrayList<>();
 
+        int K = 7;
+        int[] A = {1, 2, 3, 4, 5};
+        int N = A.length;
+
+        for (int i = 0; i < K; i++){
+            int temp = A[A.length - 1];
+            for (int j = N - 1; j > 0; j--) {
+                A[j] = A[j - 1];
+            }
+            A[0] = temp;
+        }
+        System.out.println(Arrays.toString(A));
 
     }
+
 }
