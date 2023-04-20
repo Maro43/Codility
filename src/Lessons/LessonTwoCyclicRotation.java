a5957793f92a1ac49ad8d2d5aaec8abcc7b264bd
Maro 43 <mariusz.olbrys@outlook.com>
2023-04-20 12:31:14 +0200

    Lesson Two

--- src/Lessons/LessonTwoCyclicRotation.java
+++ src/Lessons/LessonTwoCyclicRotation.java
@@ -3,22 +3,29 @@ package Lessons;
 import java.util.Arrays;
 
 public class LessonTwoCyclicRotation {
-    public static void main(String[] args) {
+    public int[] solution(int[] A, int K) {
 
-
-        int K = 7;
-        int[] A = {1, 2, 3, 4, 5};
         int N = A.length;
 
+        if (N == 0)
+            return A;
         for (int i = 0; i < K; i++){
-            int temp = A[A.length - 1];
-            for (int j = N - 1; j > 0; j--) {
+            int sol = A[A.length - 1];
+            for (int j = N - 1; j > 0; j--){
                 A[j] = A[j - 1];
             }
-            A[0] = temp;
+            A[0] = sol;
         }
-        System.out.println(Arrays.toString(A));
-
+        return A;
     }
 
+    public static void main(String[] args) {
+        int[] A = {3,2,1,5,6,7,8};
+        int K = 3;
+
+        LessonTwoCyclicRotation solution = new LessonTwoCyclicRotation();
+        int[] result = solution.solution(A, K);
+
+        System.out.println(Arrays.toString(result));
+    }
 }
