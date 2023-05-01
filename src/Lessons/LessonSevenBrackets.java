45a8fab0e0b7a6598bdbd077f06348aedf6b56db
Maro 43 <mariusz.olbrys@outlook.com>
2023-05-01 22:40:58 +0200

    Lesson Seven WorkInProgres

---
+++ src/Lessons/LessonSevenBrackets.java
@@ -0,0 +1,32 @@
+package Lessons;
+
+public class LessonSevenBrackets {
+
+    public int solution(String s) {
+        s.split("");
+        if (s.length() % 2 == 0) {
+            return 0;
+        }
+
+            for (int i = 0; i < s.length(); i++) {
+                if
+                    return 1;
+
+                }
+            }
+
+
+        boolean isClosed(char bracket) {
+                if ()
+                return
+        }
+
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        String s = "{[(]()]}";
+        System.out.println(new LessonSevenBrackets().solution(s));
+    }
+}
