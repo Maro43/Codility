99d7e13cdfd54df5fe05c3bbd2c7692aac269355
Maro 43 <mariusz.olbrys@outlook.com>
2023-06-23 11:24:35 +0200

    Lesson Three Clean

--- src/Lessons/LessonThreePermMissingElem.java
+++ src/Lessons/LessonThreePermMissingElem.java
@@ -1,8 +1,6 @@
 package Lessons;
 
-
 /*
-
 An array A consisting of N different integers is given. The array contains integers in the range [1..(N + 1)], which means that exactly one element is missing.
 
         Your goal is to find that missing element.
@@ -28,19 +26,7 @@ that, given an array A, returns the value of the missing element.
         each element of array A is an integer within the range [1..(N + 1)].
 */
 
-
 public class LessonThreePermMissingElem {
-//    public int solution(int[] a) {
-//        HashSet<Integer> set = new HashSet<>();
-//        for (int number : a) {
-//            set.add(number);
-//        }
-//        int result = 0;
-//        for (int element : set) {
-//            result = element - 1;
-//        }
-//        return result;
-//    }
 
     public int solution2(int[] a){
         int sum = 0;
@@ -50,15 +36,6 @@ public class LessonThreePermMissingElem {
         int result =(((a.length+1) * ((a.length+1)+1)) / 2) - sum;
         return result;
     }
-
-//    public int solution3(int[] a) {
-//        HashSet<Object> set = new HashSet<>();
-//        for (int number : a) {
-//            set.add(number);
-//        }
-//
-//        return 0;
-//    }
 
     public static void main(String[] args) {
         int[] a = {1, 2, 3, 5, 6, 7, 8, 9, 10};
