6816467a861b05da7c35f573b51baa479ffebddd
Maro 43 <mariusz.olbrys@outlook.com>
2023-07-02 08:36:36 +0200

    Lesson Six Max Fix

--- src/Lessons/LessonSixMaxProductOfThree.java
+++ src/Lessons/LessonSixMaxProductOfThree.java
@@ -46,49 +46,9 @@ import java.util.ArrayList;
 import java.util.Comparator;
 
 public class LessonSixMaxProductOfThree {
-/*    public int solutionWrong(int[] a) {
-        int sum = 0;
-        Arrays.sort(a);
-        int[] lastThree = Arrays.copyOfRange(a, a.length - 3, a.length);
-        sum = lastThree[0] * lastThree[1] * lastThree[2];
-        return sum;
-    }*/
+
 
     public int solution(int[] a) {
-        ArrayList<Integer> list = new ArrayList<>();
-        int result = Integer.MIN_VALUE;
-        int sum;
-
-        for (int number : a) {
-            list.add(number);
-        }
-        list.sort(Comparator.naturalOrder());
-        for (int i = 0; i < list.size() - 2; i++) {
-            for (int j = i + 1; j < list.size() - 1; j++) {
-                sum = list.get(i) * list.get(j) * list.get(list.size() - 1);
-                result = Math.max(sum, result);
-            }
-
-        }
-        return result;
-    }
-
-    public int solution2(int[] a) {
-        int x = a.length;
-        int result = Integer.MIN_VALUE;
-        int sum;
-        for (int i = 0; i < x - 2; i++) {
-            for (int j = i + 1; j < x - 1; j++) {
-                for (int k = j + 1; k < x; k++) {
-                    sum = a[i] * a[j] * a[k];
-                    result = Math.max(sum, result);
-                }
-            }
-        }
-        return result;
-    }
-
-    public int solution3(int[] a) {
         ArrayList<Integer> list = new ArrayList<>();
 
 
@@ -108,15 +68,12 @@ public class LessonSixMaxProductOfThree {
         return result;
     }
 
-//            set.add(a);
-//   set.stream().sorted().
-//            sum = a[0]*a[1]*a[2];
-//
 
 
     public static void main(String[] args) {
         int[] a = {-3, 1, 2, -2, 5, 6};
         LessonSixMaxProductOfThree productOfThree = new LessonSixMaxProductOfThree();
-        System.out.println(productOfThree.solution3(a));
+        System.out.println(productOfThree.solution(a));
     }
+
 }
