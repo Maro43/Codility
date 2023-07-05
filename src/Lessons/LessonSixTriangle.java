9a8c2579f3a453440f40bdf1907dadda573f2ffc
Maro 43 <mariusz.olbrys@outlook.com>
2023-07-05 08:40:14 +0200

    Lesson Six Triangle fix

--- src/Lessons/LessonSixTriangle.java
+++ src/Lessons/LessonSixTriangle.java
@@ -56,26 +56,7 @@ public class LessonSixTriangle {
         }
         return 0;
     }
-    public int solution2(int[] a) {
-        ArrayList<Integer> list = new ArrayList<>();
-        for (int i = 0; i < a.length; i++) {
-            list.add(a[i]);
-        }
-        list.sort(Comparator.naturalOrder());
-        for (int i = 0; i < list.size()-2; i++) {
-            int sumC = list.get(i) + list.get(i+1);
-            int sumA = list.get(i+1) + list.get(i+2);
-            int sumB = list.get(i) + list.get(i+2);
 
-            if (sumC > a[i + 2]) {
-                if (sumB > a[i + 1]) {
-                    if (sumA > a[i])
-                        return 1;
-                }
-            }
-        }
-        return 0;
-    }
 
     public static void main(String[] args) {
         int[] a = {Integer.MAX_VALUE,Integer.MAX_VALUE,Integer.MAX_VALUE};
