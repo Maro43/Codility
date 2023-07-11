b8c36a6b7cde1280c0aebd9be9a4c84655f19bdd
Maro 43 <mariusz.olbrys@outlook.com>
2023-07-11 08:50:56 +0200

    Lesson Seven Brackets fix

--- src/Lessons/LessonSevenBrackets.java
+++ src/Lessons/LessonSevenBrackets.java
@@ -28,13 +28,13 @@ import java.util.Stack;
 
 public class LessonSevenBrackets {
 
-    public int solution3(String s) {
+    public int solution(String s) {
         HashMap<String, String> map = new HashMap<>();
         map.put("}", "{");
         map.put("]", "[");
         map.put(")", "(");
-        Stack<String> stack = new Stack<>();
 
+        Stack<String> stack = new Stack<>();
         if (s.length() % 2 != 0)
             return 0;
 
@@ -56,80 +56,12 @@ public class LessonSevenBrackets {
         return 0;
     }
 
-    public int solution2(String s) {
-        s.split("");
-        if (s.length() % 2 != 0) {
-            return 0;
-        }
-        Stack<Character> stack = new Stack<>();
 
-        for (int i = 0; i < s.length(); i++) {
-            char save = s.charAt(i);
-            if (save == '{' || save == '[' || save == '(') {
-                stack.push(save);
-            } else {
-                if (stack.size() == 0) return 0;
 
-                char check = stack.pop();
-                if ((check == '{' && save == '}') || (check == '[' && save == ']') || (check == '(' && save == ')')) ;
-                else return 0;
 
-            }
-        }
-        if (stack.size() == 0)
-            return 1;
-
-        return 0;
-    }
-
-    public int solution(String s) {
-        if (s.length() % 2 != 0) {
-            return 0;
-        }
-        Stack<Character> stack = new Stack<>();
-
-        for (int i = 0; i < s.length(); i++) {
-            char br = s.charAt(i);
-            if (isStart(br)) {
-                stack.push(br);
-            } else {
-                if (stack.size() == 0) {
-                    return 0;
-                }
-                char br1 = stack.pop();
-                if (isClosed(br1, br) == false) {
-                    return 0;
-                }
-
-            }
-        }
-        if (stack.size() == 0)
-            return 1;
-        return 0;
-    }
-
-    boolean isStart(char bracket) {
-        if (bracket == '{' || bracket == '[' || bracket == '(') {
-            return true;
-        }
-        return false;
-    }
-
-    boolean isClosed(char bracket1, char bracket2) {
-        if (bracket1 == '{' && bracket2 == '}') {
-            return true;
-        }
-        if (bracket1 == '[' && bracket2 == ']') {
-            return true;
-        }
-        if (bracket1 == '(' && bracket2 == ')') {
-            return true;
-        }
-        return false;
-    }
 
     public static void main(String[] args) {
         String s = "{[(()(())())]}";
-        System.out.println(new LessonSevenBrackets().solution3(s));
+        System.out.println(new LessonSevenBrackets().solution(s));
     }
 }
