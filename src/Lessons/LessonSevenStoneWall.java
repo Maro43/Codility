3ed0938ca289333dc4570e6e7b3425a8aece0dba
Maro 43 <mariusz.olbrys@outlook.com>
2023-07-19 08:56:58 +0200

    Lesson Seven Stone clean

--- src/Lessons/LessonSevenStoneWall.java
+++ src/Lessons/LessonSevenStoneWall.java
@@ -1,7 +1,6 @@
 package Lessons;
 
 /*
-
 You are going to build a stone wall. The wall should be straight and N meters long, and its thickness should be constant; however, it should have different heights in different places. The height of the wall is specified by an array H of N positive integers. H[I] is the height of the wall from I to I+1 meters to the right of its left end. In particular, H[0] is the height of the wall's left end and H[N−1] is the height of the wall's right end.
 
 The wall should be built of cuboid stone blocks (that is, all sides of such blocks are rectangular). Your task is to compute the minimum number of blocks needed to build the wall.
@@ -23,7 +22,6 @@ Write an efficient algorithm for the following assumptions:
 
         N is an integer within the range [1..100,000];
         each element of array H is an integer within the range [1..1,000,000,000].
-
 */
 
 import java.util.Stack;
@@ -31,10 +29,10 @@ import java.util.Stack;
 public class LessonSevenStoneWall {
 
     public int solution(int[] h) {
-
         Stack<Integer> stack = new Stack<>();
         int stones = 0;
         int currentStone;
+
         for (int i = 0; i < h.length; i++) {
             currentStone = h[i];
             while (!stack.isEmpty()&&currentStone<stack.peek()) {
@@ -52,7 +50,6 @@ public class LessonSevenStoneWall {
         }
         return stones;
     }
-
 
     public static void main(String[] args) {
         int[] h = {8, 8, 5, 7, 9, 8, 7, 5, 8};
